8d16747239bfe592726ecad00eb821f1f5e0b503
muyong <whatissrc@gmail.com>
2020-09-08 16:21:21 +0800

    create project

---
+++ expend_system/src/main/java/com/lhstudy/expend_system/ExpendSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lhstudy.expend_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpendSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpendSystemApplication.class, args);
+    }
+
+}
